9b1413ca9d630aa6daf8fa3bf18fc9f8b4333168
Abdullah Qayyum <bsef19a039@pucit.edu.pk>
2022-04-29 12:10:32 +0500

    changed application icon and name

--- Calculator_39/src/main/java/com/example/myapplication/MainActivity.java
+++ Calculator_39/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String expression = "";
+    TextView resultContainer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView _resultContainer = findViewById(R.id.resultContainer);
+        resultContainer=_resultContainer;
+
+    }
+
+    public void setContainerValue(View view) {
+        expression += ( (TextView) view ).getText().toString();
+        Toast.makeText(this, expression, Toast.LENGTH_SHORT).show();
+        resultContainer.setText(expression);
+    }
+    public void EvaluateExpression(View view){
     }
 }
