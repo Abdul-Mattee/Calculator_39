a468f577130b98123efb9d7c9c59a7d8201fafa5
Abdullah Qayyum <bsef19a039@pucit.edu.pk>
2022-04-30 16:37:04 +0500

    landscape layout  + functionality created

--- Calculator_39/src/main/java/com/example/myapplication/MainActivity.java
+++ Calculator_39/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,30 +1,49 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.itis.libs.parserng.android.expressParser.MathExpression;
 
 public class MainActivity extends AppCompatActivity {
 
     public String expression = "";
+    public String answer="";
     TextView resultContainer;
+    TextView expressionContainer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView _resultContainer = findViewById(R.id.resultContainer);
+        TextView _expressionContainer = findViewById(R.id.expressionContainer);
         resultContainer=_resultContainer;
+        expressionContainer=_expressionContainer;
 
     }
-
-    public void setContainerValue(View view) {
+    public void ClearExpression(View view){
+        expression="";
+        expressionContainer.setText(expression);
+    }
+    public void SetContainerValue(View view) {
         expression += ( (TextView) view ).getText().toString();
-        Toast.makeText(this, expression, Toast.LENGTH_SHORT).show();
-        resultContainer.setText(expression);
+        expressionContainer.setText(expression);
+    }
+    public void DeleteExpression(View view){
+        if(expression.length()<=0) return;
+        expression = expression.substring(0,expression.length()-1);
+        expressionContainer.setText(expression);
+    }
+    public void ShowAnswer(View view){
+        expression+=answer;
+        expressionContainer.setText(expression);
     }
     public void EvaluateExpression(View view){
+        MathExpression exp = new MathExpression(expression);
+        answer= exp.solve();
+        resultContainer.setText(answer);
     }
 }
